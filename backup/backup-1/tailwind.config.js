d82fd9782f48c223059772317ee6743a8185c930
milesmf <milesfalk@gmail.com>
2026-01-25 19:39:37 +0000

    $

--- backup/backup-1/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  prefix: 'afm-',
-  corePlugins: { preflight: false },
-  content: ['./index.html', './**/*.html', './**/*.js'],
-  theme: { extend: {} },
-  plugins: [],
-};
